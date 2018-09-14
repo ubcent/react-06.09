b2bc1b760f87b9f6d645f69a8b410dc23a5ee5a8
fufylev <fufylev@gmail.com>
2018-09-14 17:08:59 +0300

    repeated code from lesson 3

--- Andrei_Fufylev/src/index.jsx
+++ Andrei_Fufylev/src/index.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 
+import Header from 'components/Header'
+
+const creators =['Vasya Pupkin', 'Andrei Fufylev', 'Dmitrii Bondarchuk'];
+
 class App extends Component {
+    handleClick() {
+        console.log('button click');
+    }
     render() {
         return (
-            <div>Hello world!!!</div>
+            <div>
+                <Header onButtonClick={this.handleClick} size='small' creators={creators}>
+                <div>
+                    Hello, I'm a child of Header
+                </div>
+                </Header>
+        
+                Hello world!!!
+                </div>
         );
     }
 }
