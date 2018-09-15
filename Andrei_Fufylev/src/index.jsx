b3853fff8628f7fa604a313a56d8c04a42527207
fufylev <fufylev@gmail.com>
2018-09-15 13:32:11 +0300

    Added react-bootstrap. Altered all components with bootstrap

--- Andrei_Fufylev/src/index.jsx
+++ Andrei_Fufylev/src/index.jsx
@@ -1,66 +1,42 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 
-import './components/Style.css';
+import './components/Layout/MainPage/Style.css';
 
-import Header from 'components/Header';
-import Login from 'components/Login';
-import Menu from 'components/Menu';
-
-//const creators =['Vasya Pupkin', 'Andrei Fufylev', 'Dmitrii Bondarchuk'];
-const menuItems = [
-    {
-        title: 'Home',
-        href: '#',
-        itemClass: 'nav-item',
-        linkClass: 'nav-link active'
-    },
-    {
-        title: 'Articles',
-        href: '#',
-        itemClass: 'nav-item',
-        linkClass: 'nav-link'
-    },
-    {
-        title: 'Blog',
-        href: '#',
-        itemClass: 'nav-item',
-        linkClass: 'nav-link'
-    },
-    {
-        title: 'Contacts',
-        href: '#',
-        itemClass: 'nav-item',
-        linkClass: 'nav-link disabled'
-    },
-];
+import Header from 'components/Layout/MainPage/Header';
+import Login from 'components/Layout/MainPage/Login';
+import Menu from 'components/Layout/MainPage/Menu';
+import Articles from 'components/Layout/MainPage/Articles';
 
 class App extends Component {
-    
+
     render() {
         return (
             <div className='wrapper'>
                 <div className='container'>
+                
                     <div className="row">
                         <div className="col-12">
                             <Header></Header>
                         </div>
-                        
+
                     </div>
                     <hr />
                     <div className="row">
                         <div className="col-8">
-                            <Menu menuItems={menuItems}></Menu>
+                            <Menu />
                         </div>
                         <div className="col-4 text-right">
-                            <Login></Login>
+                            <Login buttonLabel='Login'></Login>
                         </div>
                     </div>
                     <hr />
-                    <div className="row"></div>
+                    <div>
+                        <Articles ></Articles>
+                    </div>
                 </div>
             </div>
-            
+
         );
     }
 }
