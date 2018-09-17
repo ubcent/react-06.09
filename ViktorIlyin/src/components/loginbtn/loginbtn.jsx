5f59690e7597d563611984173d365c747d39cdd8
Vikenter <vikente.med@gmail.com>
2018-09-17 14:44:11 +0300

    ViktorIlyinDZ03

---
+++ ViktorIlyin/src/components/loginbtn/loginbtn.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class LoginBtn extends Component {
+    render() {
+        return (
+            <button>login</button>
+        );
+    }
+}
